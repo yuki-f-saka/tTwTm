4e38b9353cb8bf272518fc16e85ce90906f69c9d
Yuki Funasaka <yuki.funasaka@uhuru.jp>
2022-06-06 22:46:00 +0900

    HelloFunasakaWorldを作成

---
+++ src/components/HelloFunasakaWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings frame-line-yellow">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>This Area is Funasaka's Area.</h3>
+  </div>
+</template>
+
+<style scoped>
+.frame-line-yellow {
+    border: yellow 1rem dotted;
+}
+</style>
--- src/pages/example.vue
+++ src/pages/example.vue
@@ -1,7 +1,10 @@
+<script setup>
+import HelloFunasakaWorld from '../components/HelloFunasakaWorld.vue';
+</script>
+
 <template>
   <div>
     <h1>I'm example</h1>
+    <HelloFunasakaWorld msg="Hello Funasaka World!"/>
   </div>
 </template>
-<script setup>
-</script>
