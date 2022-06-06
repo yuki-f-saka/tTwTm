bd3bea1afe809664ccc82807f7f3bc90b885ca33
Yuki Funasaka <yuki.funasaka@uhuru.jp>
2022-06-06 22:50:50 +0900

    example.vueに説明を付け足した

--- src/pages/example.vue
+++ src/pages/example.vue
@@ -3,8 +3,15 @@ import HelloFunasakaWorld from '../components/HelloFunasakaWorld.vue';
 </script>
 
 <template>
-  <div>
+  <div class="example-component">
     <h1>I'm example</h1>
+    <span>This is example component area. Please make some components HERE!</span>
     <HelloFunasakaWorld msg="Hello Funasaka World!"/>
   </div>
 </template>
+
+<style scoped>
+.example-component {
+  border: grey 1px solid;
+}
+</style>
